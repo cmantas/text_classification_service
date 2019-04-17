7e058f4272bb24ab92833340655842ec1bbc0d68
Christos Mantas <the1pro@gmail.com>
2019-04-17 20:06:49 +0300

    Create Conv1DModel
    A convolutional Text model with a 1D convolution.

--- ml_models/__init__.py
+++ ml_models/__init__.py
@@ -5,3 +5,4 @@ from ml_models.word_level_model import WordLevelModel
 # non-abstract models
 from ml_models.feed_forward_model import FeedForwardModel
 from ml_models.lstm_model import LSTMModel
+from ml_models.conv_1d_model import Conv1DModel
---
+++ ml_models/conv_1d_model.py
@@ -0,0 +1,41 @@
+from keras.layers import Dense, Embedding, Conv1D, GlobalMaxPooling1D, Dropout
+from keras.models import Sequential
+from keras.preprocessing.sequence import pad_sequences
+
+from ml_models import WordLevelModel, MultiClassModel
+
+
+class Conv1DModel(MultiClassModel, WordLevelModel):
+    VOCAB_SIZE = 30000
+    BATCH_SIZE = 4000
+    MAX_SEQ_LEN = 100
+
+    @classmethod
+    def model_description(cls, encoder):
+        num_labels = len(encoder.classes_)
+        embedding_dim = 128
+        num_filters = 20
+        filter_size = 5
+        layers = [
+            Embedding(cls.VOCAB_SIZE, embedding_dim, input_length=cls.MAX_SEQ_LEN),
+            Conv1D(num_filters, filter_size, activation='relu'),
+            GlobalMaxPooling1D(),
+            # Dropout(0.1),
+            Dense(20, activation='relu'),
+            Dropout(0.05),
+            Dense(num_labels, activation='sigmoid'),
+        ]
+
+        model = Sequential(layers)
+
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        return model
+
+    def vectorize_texts(self, texts):
+        seqs = self.tokenizer.texts_to_sequences(texts)
+        X = pad_sequences(seqs, maxlen=self.MAX_SEQ_LEN)
+        return X
+
+
