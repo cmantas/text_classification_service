fed577d80dcac8b33f490660451e0b5d325d81a2
Christos Mantas <the1pro@gmail.com>
2019-06-13 20:05:33 +0300

    Create `/saved_models` endpoint
    List all available models.

--- ml_models/helpers.py
+++ ml_models/helpers.py
@@ -1,4 +1,7 @@
 from random import shuffle
+from os import listdir
+from os.path import isfile, join, split
+import pickle
 
 def read_data(fname, token='#'):
     with open(fname) as f:
@@ -49,3 +52,21 @@ def training_gen(texts, batch_size, tokenizer, label_encoder):
       X, Y = vectorize_batch(batch, tokenizer, label_encoder)
       yield (X, Y)
 
+
+def list_models(path):
+    rv = []
+    for f in listdir(path):
+        fpath = join(path, f)
+        if isfile(fpath) and f.endswith('.pickle'):
+            rv.append(fpath)
+    return rv
+
+
+def load_models(models_dir):
+    model_paths = list_models(models_dir)
+    rv = {}
+    for fpath in model_paths:
+        fname = split(fpath)[-1]
+        model = pickle.load(open(fpath, 'rb'))
+        rv[fname] = model
+    return rv
--- web_api.py
+++ web_api.py
@@ -2,13 +2,16 @@
 import flask
 from flask import jsonify
 from inspect import getmembers, isclass, isabstract
+from ml_models.helpers import load_models
 
 import ml_models
 
 
 app = flask.Flask(__name__)
 
-def available_models():
+SAVED_MODELS_DIRECTORY = 'data/saved_models'
+
+def list_model_types():
     models = []
     for _name, obj in getmembers(ml_models):
         if isclass(obj) and not isabstract(obj):
@@ -16,12 +19,22 @@ def available_models():
     return models
 
 
-model_classes = { m.__name__: m for m in available_models() }
+model_classes = {m.__name__: m for m in list_model_types()}
+
+# Load saved models
+saved_models = load_models(SAVED_MODELS_DIRECTORY)
+
 
 @app.route('/model_types', methods=['GET'])
 def model_types():
     rsp = list(model_classes.keys())
     # return a response in json format
+    return jsonify(rsp)
+
+
+@app.route('/saved_models', methods=['GET'])
+def list_saved_models():
+    rsp = {fname: model.describe() for fname, model in saved_models.items()}
     return jsonify(rsp)
 
 # start the flask app, allow remote connections
