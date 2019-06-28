b112b0944d4326233d19799eb4ba79f90ff8e5c3
Christos Mantas <cmantas@skroutz.gr>
2019-06-28 10:56:19 +0300

    Create `/model/<model_name>/predict` endpoint in the Web Api

--- web_api.py
+++ web_api.py
@@ -1,6 +1,6 @@
 # load Flask
 import flask
-from flask import jsonify
+from flask import jsonify, request
 from inspect import getmembers, isclass, isabstract
 from ml_models.helpers import load_models
 
@@ -36,6 +36,22 @@ def model_types():
 def list_saved_models():
     rsp = {fname: model.describe() for fname, model in saved_models.items()}
     return jsonify(rsp)
+
+
+@app.route('/model/<model_name>/predict', methods=['POST'])
+def predict(model_name):
+    params = request.json
+    if model_name not in saved_models:
+        return jsonify({ 'error': 'Model not found'})
+    if 'texts' not in params:
+        return jsonify({ 'error': 'No texts given to predict'})
+
+    texts = params['texts']
+    model = saved_models[model_name]
+    probability = params.get('probabilities', False)
+    predictions = model.predict(texts, probability)
+
+    return jsonify(predictions)
 
 # start the flask app, allow remote connections
 app.run(host='0.0.0.0', debug=True)
