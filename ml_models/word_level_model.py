281fb2f9b4050480e13f62e9da3d2f18a69435f1
Christos Mantas <mantas.chris@gmail.com>
2019-07-03 00:26:49 +0300

    Improve tokenization in WordLevelModel
    Some new delimiters were added and also a warning of
    uncessarily big VOCAB_SIZE

--- ml_models/word_level_model.py
+++ ml_models/word_level_model.py
@@ -1,16 +1,21 @@
 from keras.preprocessing.text import Tokenizer
 from ml_models import TextModel
 
+DELIMITERS = "!\"#$%&()*+,-./:;<=>?@[\]^_`{|}~'“”—…«»′"
+
 
 class WordLevelModel(TextModel):
     VOCAB_SIZE = 30_000
 
     @classmethod
     def tokenizer(cls, texts):
-        tokenizer = Tokenizer(cls.VOCAB_SIZE,
-                              filters="!\"#$%&()*+,-./:;<=>?@[\]^_`{|}~'“”—",
-                              lower=True)
+        tokenizer = Tokenizer(cls.VOCAB_SIZE, filters=DELIMITERS, lower=True)
         tokenizer.fit_on_texts(texts)
+        real_vocab_size = len(tokenizer.word_index)
+
+        if real_vocab_size < cls.VOCAB_SIZE:
+            print('You are using a VOCAB_SIZE of', cls.VOCAB_SIZE,
+                  'when you only have', real_vocab_size, 'tokens')
         return tokenizer
 
     def describe(self):
