989212f4e9b712d2678811f59e128cb19b775051
Christos Mantas <the1pro@gmail.com>
2020-02-09 19:02:32 +0200

    Create save_model and load_model in tools.models
    They save a TextModel as a single pickle file.

--- tools/models.py
+++ tools/models.py
@@ -1,13 +1,8 @@
 from os import listdir
 from os.path import isfile, join, split
 import pickle
+from tempfile import NamedTemporaryFile
 from tensorflow.keras.models import load_model as load_keras_model
-
-def load_model(prefix):
-    obj = pickle.load(open(prefix + '.pickle', 'rb'))
-    keras_model = load_keras_model(prefix + '.h5')
-    obj.model = keras_model
-    return obj
 
 def list_models(path):
     rv = []
@@ -28,3 +23,40 @@ def load_models(models_dir):
 
 def describe_models(models):
     return {fname: model.describe() for fname, model in models.items()}
+
+
+def save_model(model, filename):
+    keras_model = model.model
+    keras_model_data = serialize_keras_model(keras_model)
+    model.model = None
+    intermediate = (model, keras_model_data)
+    pickle.dump(intermediate, open(filename, 'wb'))
+    model.model = keras_model # be non-destructive
+
+def load_model(filename):
+    intermediate = pickle.load(open(filename, 'rb'))
+    model, keras_model_data = intermediate
+    keras_model = deserialize_keras_model(keras_model_data)
+    model.model = keras_model
+    return model
+
+
+def deserialize_keras_model(keras_model_data):
+    """
+    Deserializes a keras model from raw bytes
+    """
+    with NamedTemporaryFile(suffix='.h5') as tmp:
+        tmp.write(keras_model_data)
+        tmp.seek(0)
+        return load_keras_model(tmp.name)
+
+
+def serialize_keras_model(km):
+    """
+    Serializes a keras model to raw bytes
+    """
+    with NamedTemporaryFile(suffix='.h5') as tmp:
+        km.save(tmp.name)
+        tmp.seek(0)
+        data = tmp.read()
+        return data
